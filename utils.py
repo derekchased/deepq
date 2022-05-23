00b4f6021b1129f89c9d78ec0be6d6bfe8d4d163
LingkaiZhu1999 <zhulingkai123@gmail.com>
2022-05-23 11:32:01 +0200

    fix typo

--- utils.py
+++ utils.py
@@ -5,10 +5,6 @@ import torch
 import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-<<<<<<< HEAD
-
-=======
->>>>>>> part 1 done
 def preprocess(obs, env):
     """Performs necessary observation preprocessing."""
     if env in ['CartPole-v0']:
